08f81fac3fc283c0fc3ef6692773e24dbfc4d54e
PaoloDigio <101996038+PaoloDigio@users.noreply.github.com>
2024-03-13 16:50:01 +0100

    change singlebook to class

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,21 +1,28 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { Component } from "react";
 
-const SingleBook = function (props) {
-  return (
-    <Card className="h-100">
-      <Card.Img variant="top" src={props.book.img} alt="img" style={{ height: "60%" }} />
-      <Card.Body className="d-flex flex-column justify-content-between" style={{ height: "40%" }}>
-        <div className="overflow-auto">
-          <Card.Title>{props.book.title}</Card.Title>
-        </div>
-        <div>
-          <Card.Text>€ {props.book.price.toFixed(2)}</Card.Text>
-          <Button variant="primary">Compra ora!</Button>
-        </div>
-      </Card.Body>
-    </Card>
-  );
-};
+class SingleBook extends Component {
+  state = {
+    selected: false,
+  };
+
+  render() {
+    return (
+      <Card className="h-100">
+        <Card.Img variant="top" src={this.props.book.img} alt="img" style={{ height: "60%" }} />
+        <Card.Body className="d-flex flex-column justify-content-between" style={{ height: "40%" }}>
+          <div className="overflow-auto">
+            <Card.Title>{this.props.book.title}</Card.Title>
+          </div>
+          <div>
+            <Card.Text>€ {this.props.book.price.toFixed(2)}</Card.Text>
+            <Button variant="primary">Compra ora!</Button>
+          </div>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
 
 export default SingleBook;
